c93a144715fbe851ccf6aba4cc31989f8c15c443
Alejandro Sazo <embryo@virt-01.virt-01>
2024-07-13 22:27:43 -0400

    avance metodo SOR

--- ellipticsolver.h
+++ ellipticsolver.h
@@ -11,47 +11,36 @@ template <class T>
 class EllipticSolver
 {
 private:
-    /* data */
-    void buildMatrixA();
-    void buildVectorB(double (*f)(double, double), double (*g1)(double), double (*g2)(double), double (*g3)(double), double (*g4)(double));
+    Matriz<T> L;
+    Matriz<T> D;
+    Matriz<T> U;
+    void buildSystem(double (*f)(double, double), double (*g1)(double), double (*g2)(double), double (*g3)(double), double (*g4)(double));
+    void SOR();
 public:
     Matriz<T> A;
+    vector<T> b;
     T xl, xr, yb, yt, dx, dy;
     int M, N;
-    EllipticSolver(T xl, T xr, T yb, T yt, int M, int N); 
+    EllipticSolver(T xl, T xr, T yb, T yt, T dx, T dy); 
     ~EllipticSolver();
-    void solve();
+    void solve(double (*f)(double, double), double (*g1)(double), double (*g2)(double), double (*g3)(double), double (*g4)(double));
 };
 
 template <class T>
-EllipticSolver<T>::EllipticSolver(T xl, T xr, T yb, T yt, int M, int N)
+EllipticSolver<T>::EllipticSolver(T xl, T xr, T yb, T yt, T dx, T dy)
 {
     this->xl = xl;
     this->xr = xr;
     this->yb = yb;
     this->yt = yt;
-    this->M = M;
-    this->N = N;
-    this->dx = (xr - xl)/M;
-    this->dy = (yt - yb)/N;
+    this->dx = dx;
+    this->dy = dy;
+    this->M = (xr - xl) / dx;
+    this->N = (yt - yb) / dy;
     cout << "dx = " << dx << endl;
     cout << "dy = " << dy << endl;
-
-    int m = M + 1;
-    int n = N + 1;
-    vector<T> x(m);
-    vector<T> y(n);
-    
-    for(int i = 0; i < m; i++) {
-        x[i] = xl + i * dx;
-        //cout << x[i] << " ";
-    }
-    //cout << endl;
-    for(int j = 0; j < n; j++) {
-        y[j] = yb + j * dy;
-        //cout << y[j] << " ";
-    }
-    //cout << endl;
+    cout << "M = " << M << endl;
+    cout << "N = " << N << endl;
 }
 
 template <class T>
@@ -60,16 +49,75 @@ EllipticSolver<T>::~EllipticSolver()
 }
 
 template <class T>
-void EllipticSolver<T>::buildMatrixA() {
+void EllipticSolver<T>::buildSystem(double (*f)(double, double), double (*g1)(double), double (*g2)(double), double (*g3)(double), double (*g4)(double)) {
     int m = M + 1;
     int n = N + 1;
     A = Matriz<T>(m*n, m*n); // ojo, n representa y, m representa x
+    b = vector<T>(m*n);
+    T dx2 = dx * dx;
+    T dy2 = dy * dy;
+
+    vector<T> x(m);
+    vector<T> y(n);
+    
+    for(int i = 0; i < m; i++) { x[i] = xl + i * dx; }
+    for(int j = 0; j < n; j++) { y[j] = yb + j * dy; }
+    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(j == 1) {
+                A(i + j*m, i + j*m) = 1.0;
+                b[i + j*m] = g1(x[i]);
+            } else if(j == n - 2) {
+                A(i + j*m, i + j*m) = 1.0;
+                b[i + j*m] = g2(x[i]);
+            } else if(i == 0) {
+                A(i + j*m, i + j*m) = 1.0;
+                b[i + j*m] = g3(y[j]);
+            } else if(i == m - 1) {
+                A(i + j*m, i + j*m) = 1.0;
+                b[i + j*m] = g4(y[j]);
+            } else {
+                A(i + j*m, i + j*m) = -2/dx2 - 2/dy2;
+                A(i + 1 + j*m, i + j*m) = 1/dx2;
+                A(i - 1 + j*m, i + j*m) = 1/dx2;
+                A(i + j*m, i + 1 + j*m) = 1/dy2;
+                A(i + j*m, i - 1 + j*m) = 1/dy2;
+                b[i + j*m] = f(x[i], y[j]);
+            }
+        }
+    }
+
+
     cout << A << endl;
+
+    for(int i = 0; i < m*n; i++) {
+        cout << b[i] << " ";
+    }
+    cout << endl;
 }
 
 template <class T>
-void EllipticSolver<T>::solve() {
-    buildMatrixA();
+void EllipticSolver<T>::SOR() {
+    // Construir matriz D, L y U
+    D = Matriz<T>(A.nrow(), A.ncol());
+    L = Matriz<T>(A.nrow(), A.ncol());
+    U = Matriz<T>(A.nrow(), A.ncol());
+    for(int i = 0; i < A.nrow(); i++) {
+        for(int j = 0; j < A.ncol(); j++) {
+            if(i == j) { D(i, j) = A(i, j); } else { D(i, j) = 0.0; }
+            // Fila > columna es inferior
+            if(i > j) { L(i, j) = A(i, j); } else { L(i, j) = 0.0; }
+            // Fila < columna es superior
+            if(i < j) { U(i, j) = A(i, j); } else { U(i, j) = 0.0; }
+        }
+    }
+}
+
+template <class T>
+void EllipticSolver<T>::solve(double (*f)(double, double), double (*g1)(double), double (*g2)(double), double (*g3)(double), double (*g4)(double)) {
+    buildSystem(f, g1, g2, g3, g4);
+    SOR();
 }
 
 #endif // ELLIPTICSOLVER_H
--- p2.cpp
+++ p2.cpp
@@ -1,13 +1,48 @@
+#include <cmath>
 #include "ellipticsolver.h"
+
+
+//phi(x, 0) = g1(x)
+double g1(double x) {
+    return -1;
+}
+
+//phi(x, 2) = g2(x)
+double g2(double x) {
+    return -1;
+}
+
+//phi(0, y) = g3(y)
+double g3(double y) {
+    return 1;
+}
+
+//phi(3, y) = g4(y)
+double g4(double y) {
+    return 1;
+}
+
+double rho_0(double x, double y) {
+    return pow((x-1)*(x-1) + (y - 0.5)*(y - 0.5), 2) - pow((x - 1), 3) - pow((y- 0.5), 3);
+}
+
+double rho(double x, double y) {
+    double rho0 = rho_0(x, y);
+    200 * rho0 * (rho0 <= 0);
+}
+
+double f(double x, double y) {
+    return -rho(x, y);
+}
 
 int main() {
     double xl = 0;
     double xr = 3;
     double yb = 0;
     double yt = 2;
-    int M = 8;
-    int N = 5;
-    EllipticSolver ellipticsolver = EllipticSolver<double>(xl, xr, yb, yt, M, N);
-    ellipticsolver.solve();
+    double dx = 0.6;
+    double dy = 0.6;
+    EllipticSolver ellipticsolver = EllipticSolver<double>(xl, xr, yb, yt, dx, dy);
+    ellipticsolver.solve(f, g1, g2 ,g3, g4);
     return 0;
 }
